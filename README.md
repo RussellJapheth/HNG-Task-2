59b5e721d66632a619be99a88483f5858a7680f3
Russell Japheth <japheth.russell.i@gmail.com>
2022-10-28 04:12:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Here be dragons
+
+## Sample usage
+
+```js
+(async () => {
+    const response = await fetch("/");
+    console.log(await response.json());
+})();
+```
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+Flight::route(
+    '/',
+    function () {
+        Flight::json(
+            [
+                "slackUsername" => "russell",
+                "backend" => true,
+                "age" => 51,
+                "bio" => "Backend Developer | Technical Writer | Linux Advocate | Food Enthusiast"
+            ]
+        );
+    }
+);
+
+Flight::start();
